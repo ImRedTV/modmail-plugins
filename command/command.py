8fbf5ff490c28f7cb9d9236972b0fd9962db9590
RED <50785550+ImRedTV@users.noreply.github.com>
2021-04-11 17:33:27 +0200

    Update command.py

--- command/command.py
+++ command/command.py
@@ -1,56 +1,21 @@
 import discord
-from discord.ext import commands, tasks
+from discord.ext import commands
 
-import asyncio
-
-from core import checks
-from core.models import PermissionLevel
-from core.paginator import EmbedPaginatorSession
-
-import datetime
-
+class Test1(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+    
     @commands.command()
-    async def test(self, ctx):
-        if self.decay_channels:
-            pages = []
-            total = 0
-
-            for channel in self.decay_channels:
-                total += self.decay_channels[channel]
-                
-            average = total / len(self.decay_channels)
-
-            front = discord.Embed(color=self.bot.main_color, title="All decay info.")
-            front.add_field(
-                name="Decay channels:",
-                value=str(len(self.decay_channels)),
-                inline=True,
-            )
-            front.add_field(
-                name="Average decay time:",
-                value=f"{str(average)}ms",
-                inline=True,
-            )
-            front.add_field(
-                name="To see channel specific info, use the reactions below.",
-                value="\u200b",
-                inline=False,
-            )
-            pages.append(front)
-
-            for channel in self.decay_channels:
-                d_channel = self.bot.get_channel(int(channel))
-                page = discord.Embed(color=self.bot.main_color, title=f"Decay info of: #{d_channel.name}")
-                page.add_field(name="Decay time:", value=f"{str(self.decay_channels[channel])}ms")
-
-                pages.append(page)
-
-            session = EmbedPaginatorSession(ctx, *pages)
-            await session.run()    
-
-        else:
-            embed = discord.Embed(
-                color=self.bot.error_color,
-                title="No channels are decaying, to decay a channel use the command: `[p]decay #channel`",
-            )
-            await ctx.send(embed=embed)
+    async def send_s(self, ctx):
+        embed = discord.Embed(
+            title="***7 SIN'S SOCIALS***\n▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬",
+            description="<:instagram:724501733317017682> **Instagram:**\n• <https://www.instagram.com/7sinsggs/>\n\n<:youtube:663247076167122945> **YouTube:**\n• <https://www.youtube.com/channel/UCrA0fNlbVXKoA1eSu8YsYhA/featured>\n\n<:twitter:724501698248441877> **Twitter:**\n• <https://twitter.com/7sinsGGs>\n\n<:twitch:740634096446734366> **Twitch:**\n• <https://www.twitch.tv/7sinsggs>",
+            color=0xee3463,
+            timestamp=ctx.message.created_at
+        )
+        embed.set_footer(text="Management Team", icon_url="https://cdn.discordapp.com/attachments/726193232798810132/740629657191186562/7S-.gif")
+        await ctx.send(embed=embed)
+        
+        
+def setup(bot):
+    bot.add_cog(Test1(bot))
