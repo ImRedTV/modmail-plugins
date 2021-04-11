542ec2adad05ace2839fae05c703ade000aef960
RED <50785550+ImRedTV@users.noreply.github.com>
2021-04-11 16:59:54 +0200

    Update command.py

--- command/command.py
+++ command/command.py
@@ -1,11 +1,6 @@
 import discord
 from discord.ext import commands
 
-class Say(commands.Cog):
-    def __init__(self, bot):
-        self.bot = bot
-        
-    @bot.command(pass_context=True)
+@bot.command()
 async def ping(ctx):
-    embed = discord.Embed(title="Pong! :ping_pong:")
-    await bot.say(embed=embed)
+    await ctx.send('pong')
