7c603a375088584100e912ee49e2c872c8d4e29c
RED <50785550+ImRedTV@users.noreply.github.com>
2021-04-11 17:38:26 +0200

    Update command.py

--- command/command.py
+++ command/command.py
@@ -6,15 +6,13 @@ class Test1(commands.Cog):
         self.bot = bot
     
     @commands.command()
-    async def sm7b(self, ctx):
+    async def empire(self, ctx):
         embed = discord.Embed(
-            title=":blue_circle: Pourquoi déconseiller le SM7B et autres micros Broadcast Dynamiques?",
-            description="Version Courte', '\n Car ils ne sont pas adaptés et n\'amèneront que des problèmes. Les micros dynamiques sont très peu sensibles, ils nécessitent énormément de gain. Parfois 60dBs (soit amplifié 1000x), cela inclus tous les parasites et souffles des composants électroniques qui se situeront sur le chemin. Ils sont fait pour être utilisés très proches, et sont rien que pour ça très peu adaptés à l\'animation en stream.\n \n **Vous êtes l\'animateur de votre stream, vous allez gigoter dans tous les sens, faites des économies d\'argent et de migraine et prenez un statique.** \n\n [Version longue & plus d\'info...](https://discord.com/channels/333720455074676736/812345334536863844/820309055523651584)",
+            title="**Qu'est ce qu'Empire Media Science ?**",
+            description="**Empire Media Science** est un consortium de techniciens et d'artistes de l'audiovisuel et des nouveaux médias, créé par <@239744631225581578>, Directeur Technique & Artistique derrière le son et l'image d'une grande partie des grands créateurs de contenu français.",
             color=0xff0000,
             timestamp=ctx.message.created_at
         )
-        embed.set_footer(text="Management Team", icon_url="https://cdn.discordapp.com/attachments/726193232798810132/740629657191186562/7S-.gif")
-        await ctx.send(embed=embed)
         
         
 def setup(bot):
