70d3444a297c92b91b48529f44e76450ef518c57
RED <50785550+ImRedTV@users.noreply.github.com>
2021-04-11 15:24:58 +0200

    Update sell.py

--- sell/sell.py
+++ sell/sell.py
@@ -5,9 +5,9 @@ from core.models import PermissionLevel
 import asyncio
 
 
-class sell(commands.Cog):
+class Suggest(commands.Cog):
     """
-   Envoyons-nous une vente dans le canal désigné.
+    Let's you send a suggestion to a designated channel.
     """
 
     def __init__(self, bot):
@@ -33,53 +33,53 @@ class sell(commands.Cog):
 
     @commands.command()
     @checks.has_permissions(PermissionLevel.REGULAR)
-    async def sell(self, ctx, *, sellion):
+    async def suggest(self, ctx, *, suggestion):
         """
-        sell something!
+        Suggest something!
         **Usage**:
-        [p]sell more plugins!
+        [p]suggest more plugins!
         """
         if str(ctx.author.id) not in self.banlist:
             async with ctx.channel.typing():
                 config = await self.coll.find_one({"_id": "config"})
                 if config is None:
                     embed = discord.Embed(
-                        title="sellion channel not set.", color=self.bot.error_colour
+                        title="Suggestion channel not set.", color=self.bot.error_colour
                     )
                     embed.set_author(name="Error.")
                     embed.set_footer(text="Task failed successfully.")
                     await ctx.send(embed=embed)
                 else:
-                    sellion_channel = self.bot.get_channel(
-                        int(config["sellion-channel"]["channel"])
+                    suggestion_channel = self.bot.get_channel(
+                        int(config["suggestion-channel"]["channel"])
                     )
 
-                    embed = discord.Embed(title=sellion, color=0x59E9FF)
+                    embed = discord.Embed(title=suggestion, color=0x59E9FF)
                     embed.set_author(
-                        name=f"sellion by {ctx.author}:", icon_url=ctx.author.avatar_url
+                        name=f"Suggestion by {ctx.author}:", icon_url=ctx.author.avatar_url
                     )
-                    await sellion_channel.send(embed=embed)
+                    await suggestion_channel.send(embed=embed)
                     await ctx.message.add_reaction("\N{WHITE HEAVY CHECK MARK}")
         else:
             await ctx.send(embed=discord.Embed(color=self.bot.error_color, title=f"You have been blocked, {ctx.author.name}#{ctx.author.discriminator}.", description=f"Reason: {self.banlist[str(ctx.author.id)]}"))
 
     @commands.command(aliases=["ssc"])
     @checks.has_permissions(PermissionLevel.ADMIN)
-    async def setsellchannel(self, ctx, channel: discord.TextChannel):
+    async def setsuggestchannel(self, ctx, channel: discord.TextChannel):
         """
-        Set the channel where sellions go.
+        Set the channel where suggestions go.
         **Usage**:
-        [p]setsellchannel #sellions
-        [p]ssc sellions
+        [p]setsuggestchannel #suggestions
+        [p]ssc suggestions
         [p]ssc 515085600047628288
         """
         await self.coll.find_one_and_update(
             {"_id": "config"},
-            {"$set": {"sellion-channel": {"channel": str(channel.id)}}},
+            {"$set": {"suggestion-channel": {"channel": str(channel.id)}}},
             upsert=True,
         )
         embed = discord.Embed(
-            title=f"Set sellion channel to #{channel}.", color=0x4DFF73
+            title=f"Set suggestion channel to #{channel}.", color=0x4DFF73
         )
         embed.set_author(name="Success!")
         embed.set_footer(text="Task succeeded successfully.")
@@ -87,33 +87,33 @@ class sell(commands.Cog):
 
     @commands.command()
     @checks.has_permissions(PermissionLevel.ADMIN)
-    async def sellchannel(self, ctx):
-        """Displays the sellion channel."""
+    async def suggestchannel(self, ctx):
+        """Displays the suggestion channel."""
         config = await self.coll.find_one({"_id": "config"})
-        sellion_channel = self.bot.get_channel(
-            int(config["sellion-channel"]["channel"])
+        suggestion_channel = self.bot.get_channel(
+            int(config["suggestion-channel"]["channel"])
         )
         embed = discord.Embed(
-            title=f"The sellion channel is: #{sellion_channel}",
-            description="To change it, use [p]setsellchannel.",
+            title=f"The suggestion channel is: #{suggestion_channel}",
+            description="To change it, use [p]setsuggestchannel.",
             color=0x4DFF73,
         )
         await ctx.send(embed=embed)
 
     @checks.has_permissions(PermissionLevel.MOD)
     @commands.group(invoke_without_command=True)
-    async def sellmod(self, ctx: commands.Context):
-        """Let's you block and unblock people from using the sell command."""
+    async def suggestmod(self, ctx: commands.Context):
+        """Let's you block and unblock people from using the suggest command."""
         await ctx.send_help(ctx.command)
 
-    @sellmod.command(aliases=["ban"])
+    @suggestmod.command(aliases=["ban"])
     @checks.has_permissions(PermissionLevel.MOD)
     async def block(self, ctx, user: discord.User, *, reason="Reason not specified."):
         """
-        Block a user from using the sell command.
+        Block a user from using the suggest command.
         **Examples:**
-        [p]sellmod block @RealCyGuy for abuse!
-        [p]sellmod ban 543225108135673877 `cause he's the same person!!!
+        [p]suggestmod block @RealCyGuy for abuse!
+        [p]suggestmod ban 543225108135673877 `cause he's the same person!!!
         """
         if str(user.id) in self.banlist:
             embed = discord.Embed(
@@ -132,14 +132,14 @@ class sell(commands.Cog):
         await self._update_mod_db()
         await ctx.send(embed=embed)
 
-    @sellmod.command(aliases=["unban"])
+    @suggestmod.command(aliases=["unban"])
     @checks.has_permissions(PermissionLevel.MOD)
     async def unblock(self, ctx, user: discord.User):
         """
-        Unblock a user from using the sell command.
+        Unblock a user from using the suggest command.
         **Examples:**
-        [p]sellmod unblock @RealCyGuy
-        [p]sellmod unban 543225108135673877
+        [p]suggestmod unblock @RealCyGuy
+        [p]suggestmod unban 543225108135673877
         """
         if str(user.id) not in self.banlist:
             embed = discord.Embed(
@@ -158,4 +158,4 @@ class sell(commands.Cog):
 
 
 def setup(bot):
-    bot.add_cog(sell(bot))
+    bot.add_cog(Suggest(bot))
