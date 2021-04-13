4d24403e6629774b434aaefbdfacbd3e4f0861a6
RED <50785550+ImRedTV@users.noreply.github.com>
2021-04-13 14:32:33 +0200

    Update command.py

--- command/command.py
+++ command/command.py
@@ -7,7 +7,7 @@ class Test1(commands.Cog):
     
     @commands.command()
     async def empire(self, ctx):
-        await client.delete_message(message)
+        await client.delete_message(message):
         embed = discord.Embed(
             title="**Qu'est ce qu'Empire Media Science ?**",
             description="**Empire Media Science** est un consortium de techniciens et d\'artistes de l\'audiovisuel et des nouveaux médias, créé par <@!239744631225581578>, Directeur Technique & Artistique derrière le son et l\'image d\'une grande partie des grands créateurs de contenu français.\n\n*Si vous êtes venu ici pour savoir comment on a fait le son ou l\'image de LeBouseuh ou Inoxtag,* **Vous êtes au bon endroit !**\n\nN\'hésitez pas a visiter notre <#812345334536863844> et nos <#812775265763328000> ou nous poser vos questions dans <#751028848874749962> ou <#813813822116724746>. Nous vous aiderons du mieux qu\'on peut !\n\nAmusez-vous bien et n\'oubliez pas de lire le <#763141753351045152>",
