1c3a028528e8b9ac8df0afc999d83e8336c2bcec
RED <50785550+ImRedTV@users.noreply.github.com>
2021-04-13 23:23:41 +0200

    Update command.py

--- command/command.py
+++ command/command.py
@@ -26,6 +26,28 @@ class Test1(commands.Cog):
 
         embed.set_thumbnail(url="https://i.imgur.com/JpsKGgy.png")
         await ctx.send(embed=embed)
+        
+        @commands.command()
+    async def dynamique(self, ctx):
+        await ctx.message.delete()#suprime l'appel
+        embed = discord.Embed(
+            title=":blue_circle: Pourquoi déconseiller le SM7B et autres micros 'Broadcast Dynamiques' ?",
+            description="Version Courte', '\n Car ils ne sont pas adaptés et n\'amèneront que des problèmes. Les micros dynamiques sont très peu sensibles, ils nécessitent énormément de gain. Parfois 60dBs (soit amplifié 1000x), cela inclus tous les parasites et souffles des composants électroniques qui se situeront sur le chemin. Ils sont fait pour être utilisés très proches, et sont rien que pour ça très peu adaptés à l\'animation en stream.\n \n **Vous êtes l\'animateur de votre stream, vous allez gigoter dans tous les sens, faites des économies d\'argent et de migraine et prenez un statique.** \n\n [Version longue & plus d\'info...](https://discord.com/channels/333720455074676736/812345334536863844/820309055523651584)",
+            color=0xffc000)
+
+        embed.set_thumbnail(url="https://i.imgur.com/JpsKGgy.png")
+        await ctx.send(embed=embed)
+        
+        @commands.command()
+    async def dynamic(self, ctx):
+        await ctx.message.delete()#suprime l'appel
+        embed = discord.Embed(
+            title=":blue_circle: Pourquoi déconseiller le SM7B et autres micros 'Broadcast Dynamiques' ?",
+            description="Version Courte', '\n Car ils ne sont pas adaptés et n\'amèneront que des problèmes. Les micros dynamiques sont très peu sensibles, ils nécessitent énormément de gain. Parfois 60dBs (soit amplifié 1000x), cela inclus tous les parasites et souffles des composants électroniques qui se situeront sur le chemin. Ils sont fait pour être utilisés très proches, et sont rien que pour ça très peu adaptés à l\'animation en stream.\n \n **Vous êtes l\'animateur de votre stream, vous allez gigoter dans tous les sens, faites des économies d\'argent et de migraine et prenez un statique.** \n\n [Version longue & plus d\'info...](https://discord.com/channels/333720455074676736/812345334536863844/820309055523651584)",
+            color=0xffc000)
+
+        embed.set_thumbnail(url="https://i.imgur.com/JpsKGgy.png")
+        await ctx.send(embed=embed)
      
         
 def setup(bot):
