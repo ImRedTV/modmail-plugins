35e4227773e0631e6ddf324ea6d2dd86bb29b181
RED <50785550+ImRedTV@users.noreply.github.com>
2021-05-07 12:19:43 +0200

    Update hello.py

--- hello/hello.py
+++ hello/hello.py
@@ -11,71 +11,49 @@ class HelloPlugin(commands.Cog):
         if message.author.bot:
             return
       
-        #hello
+        #Hello
         if message.content.startswith("hello"): 
-            await message.channel.send("Hello")
             await message.add_reaction("👋")
         elif message.content.startswith("Hello"):
-            await message.channel.send("Hello !")
             await message.add_reaction("👋")
         elif message.content.startswith("HELLO"):
-            await message.channel.send("hello")
             await message.add_reaction("👋")
-        elif message.content.startswith("hemlo"):
-            await message.channel.send("hello")
-            await message.add_reaction("👋")    
-         
-         #hi   
-        elif message.content.startswith("hi"):
-            await message.channel.send("hi!")
-            await message.add_reaction("✌️")
-        elif message.content.startswith("Hi"):
-            await message.channel.send("Hey!")
-            await message.add_reaction("✌️")
+            
+        #Salut
+        if message.content.startswith("Salut"): 
+            await message.add_reaction("👋")
+        elif message.content.startswith("salut"):
+            await message.add_reaction("👋")
+                      
+       #Bonjour
+       elif message.content.startswith("bonjour"):
+            await message.add_reaction("👋")
+       elif message.content.startswith("Bonjour"):
+            await message.add_reaction("👋")
+            
+       #Bonsoir
+       elif message.content.startswith("bonsoir"):
+            await message.add_reaction("👋")
+       elif message.content.startswith("Bonsoir"):
+            await message.add_reaction("👋") 
         
-         # good moring 🌅
-        elif message.content.startswith("good morning"):
-            await message.channel.send("Good Morning!")
-            await message.add_reaction("🌅")
-        elif message.content.startswith("Good morning"):
-            await message.channel.send("Good Morning !")
-        elif message.content.startswith("gm"):
-            await message.channel.send("Good Morning!")
-            await message.add_reaction("🌅")
-        elif message.content.startswith("Gm"):
-            await message.channel.send("Good Morning!")
-        elif message.content.startswith("ji em"):
-            await message.channel.send("Ji em vai!")
-            await message.add_reaction("<:pranam:828272042402381844>")
+       #Bonne nuit
+       elif message.content.startswith("bonne nuit"):
+            await message.add_reaction("👋")
+       elif message.content.startswith("Bonne nuit"):
+            await message.add_reaction("👋") 
+        
+      #Bienvenue
+       elif message.content.startswith("bienvenue"):
+            await message.add_reaction("👋")
+       elif message.content.startswith("Bienvenue"):
+            await message.add_reaction("👋")  
 
-         # goood night    🌃
-        elif message.content.startswith("good night"):
-            await message.channel.send("Good Night!")
-            await message.add_reaction("🌃")
-        elif message.content.startswith("Good night"):
-            await message.channel.send("Good Night!") 
-            await message.add_reaction("🌃")
-        elif message.content.startswith("gn"):
-            await message.channel.send("Good Night!")
-            await message.add_reaction("🌃")
-        elif message.content.startswith("Gn"):
-            await message.channel.send("Good Night!")
-            await message.add_reaction("🌃")
-        elif message.content.startswith("ji en"):
-            await message.channel.send("Ji en vai!")
-            await message.add_reaction("<:pranam:828272042402381844>") 
-        elif '<@!474255126228500480>' in message.content.lower():
-            print("aniket")
-            await message.add_reaction('<a:swag:791539777437433876>')     
-        elif '<@!488738167969546272>' in message.content.lower():
-            if message.guild.id == 665842728545943552:
-                await message.add_reaction('<a:ohh:828312958769692692>')    
-        elif '<@!686445028469768201>' in message.content.lower():
-            if message.guild.id == 686445229687177228:
-                await message.add_reaction('<a:ohh:828312958769692692>')  
-        elif '<@!634028003114090526>' in message.content.lower():
-            if message.guild.id == 686445229687177228:
-                await message.add_reaction('<a:cookiee:829594080756498452>')          
-
+      #Welcome
+       elif message.content.startswith("Welcome"):
+            await message.add_reaction("👋")
+       elif message.content.startswith("welcome"):
+            await message.add_reaction("👋")      
+            
 def setup(bot):
     bot.add_cog(HelloPlugin(bot))
