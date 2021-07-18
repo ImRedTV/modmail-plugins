a0138b38bd2c429363ae7b465bc48cce1eda5816
RED <50785550+ImRedTV@users.noreply.github.com>
2021-07-18 16:40:21 +0200

    Update assistance.py

--- assistance/assistance.py
+++ assistance/assistance.py
@@ -12,7 +12,7 @@ class Commande(commands.Cog):
         await ctx.message.delete()#suprime l'appel
         embed = discord.Embed(
             title="**Information de connecxion**",
-            description="```Utilisez la console FiveM.\n - F8 dans le menu\n - play.sunnyisland.fr:30120 (Whitelist)```",
+            description="Utilisez la console FiveM.\n - F8 dans le menu\n - play.sunnyisland.fr:30120 (Whitelist)",
             color=0x93D929)
         
         await ctx.send(embed=embed)
