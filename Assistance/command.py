b04d65fa263127b7f1b2720ba90a3eb35e4e4e4c
RED <50785550+ImRedTV@users.noreply.github.com>
2021-07-18 16:34:31 +0200

    Create command.py

---
+++ Assistance/command.py
@@ -0,0 +1,19 @@
+import discord
+from discord.ext import commands
+from core import checks
+from core.models import PermissionLevel
+
+class Commande(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+    
+    @commands.command()
+    async def ticket(self, ctx):
+        await ctx.message.delete()#suprime l'appel
+        embed = discord.Embed(
+            title="**Information de connecxion**",
+            description="```Utilisez la console FiveM.\n - F8 dans le menu\n - play.sunnyisland.fr:30120 (Whitelist)```",
+            color=0x93D929)
+       
+        embed.set_thumbnail(url="https://i.imgur.com/Ln7dMfn.png")
+        await ctx.send(embed=embed)
